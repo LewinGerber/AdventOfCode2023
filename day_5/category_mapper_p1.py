39d1c1020134b99a9853245da03dc77b383a6f9b
Gerber, Lewin <57451950+LewinGerber@users.noreply.github.com>
2023-12-06 22:08:19 +0100

    use online solution for day five 😓️

--- day_5/category_mapper_p1.py
+++ day_5/category_mapper_p1.py
@@ -1,7 +1,6 @@
 def map_seed_to_location():
     with open("input.txt") as input:
         input_lines = input.readlines()
-        current_mapper_index = -1
         mappers: list[dict] = []
 
         for line in input_lines[1:]:
@@ -15,22 +14,27 @@ def map_seed_to_location():
 
         # resolve mappers
         seeds: list[str] = input_lines[0].split(':')[1].strip().split()
-        for mapper in mappers:
-            print(mapper)
-        print("/ // / / /  //  // /")
+        #for mapper in mappers:
+        #    print(mapper)
+        # print("<< SEEDS >>")
 
+        locations: list[int] = []
         for seed in seeds:
             current_mapped_value = int(seed)
             for mapper in mappers:
                 if current_mapped_value in mapper.keys():
                     current_mapped_value = mapper[current_mapped_value]
-
+            locations.append(current_mapped_value)
             print(current_mapped_value)
+        print("MIN: " + str(min(locations)))
+
+
+
 
 def extend_dict_by_instruction(mapper: dict, instruction: str) -> dict:
     instruction_parts = instruction.split()
-    destination_range_start = int(instruction_parts[1])
-    key_range_start = int(instruction_parts[0])
+    destination_range_start = int(instruction_parts[0])
+    key_range_start = int(instruction_parts[1])
     range_size = int(instruction_parts[2])
 
     for index in range(range_size):
